e8d5f2181f5ea81a1f8f0dca338e6a77ac76ea1a
KajetanPawlowski <101258758+KajetanPawlowski@users.noreply.github.com>
2023-03-31 09:26:19 +0200

    cascd

---
+++ src/PokedexDisplay.js
@@ -0,0 +1,15 @@
+import "./frontpage.css";
+
+import PokemonBox from "./PokemonBox";
+
+export default function PokedexDisplay() {
+    return(<div className="pokedexDisplay">
+        <PokemonBox />
+        <PokemonBox />
+        <PokemonBox />
+        <PokemonBox />
+        <PokemonBox />
+        <PokemonBox />
+    </div>
+    );
+}
