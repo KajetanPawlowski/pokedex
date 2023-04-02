0ae4fd178fd9bf1008a03cd1c018ad31ad11df69
KajetanPawlowski <101258758+KajetanPawlowski@users.noreply.github.com>
2023-04-02 13:20:56 +0200

    wwww

--- src/PokedexDisplay.js
+++ src/PokedexDisplay.js
@@ -3,33 +3,9 @@ import "./frontpage.css";
 import PokemonBox from "./PokemonBox";
 
 export default function PokedexDisplay() {
-    return(<div id="display"    className="pokedexDisplay">
-        <PokemonBox />
-        <PokemonBox />
-        <PokemonBox />
-        <PokemonBox />
-        <PokemonBox />
-        <PokemonBox />
-        <PokemonBox />
-        <PokemonBox />
-        <PokemonBox />
-        <PokemonBox />
-        <PokemonBox />
-        <PokemonBox />
-        <PokemonBox />
-        <PokemonBox />
-        <PokemonBox />
-        <PokemonBox />
-        <PokemonBox />
-        <PokemonBox />
-        <PokemonBox />
-        <PokemonBox />
-        <PokemonBox />
-        <PokemonBox />
-        <PokemonBox />
-        <PokemonBox />
-        <PokemonBox />
-        <PokemonBox />
+    return(<div id="display"  className="pokedexDisplay">
+        <PokemonBox name="pikachu"/>
+        
         
     </div>
     );
