fc539362dc1f8456c02fc82959d3c2512dbb56a6
KajetanPawlowski <101258758+KajetanPawlowski@users.noreply.github.com>
2023-04-03 18:42:42 +0200

    hgchgc

--- src/PokedexDisplay.js
+++ src/PokedexDisplay.js
@@ -1,16 +1,51 @@
+
+
+import React, { useState, useEffect } from 'react';
+import PokemonBox from './PokemonBox';
 import "./frontpage.css";
 
-import PokemonBox from "./PokemonBox";
-
 export default function PokedexDisplay() {
-    const pokemonNumbers = Array.from(Array(20), (_, i) => i + 1);
-    pokemonNumbers.forEach(element => element = element+20)
+    const [pokemonData, setPokemonData] = useState([]);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [visiblePokemon, setVisiblePokemon] = useState([])
+    const pokemonPerPage = 12;
 
-    return(<div id="display"  className="pokedexDisplay">
-        {pokemonNumbers.map(number => (
-        <PokemonBox key={number} name={number.toString()} />
-      ))}
-     
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=99')
+      .then(response => response.json())
+      .then(data => {
+        setPokemonData(data.results)
+        setVisiblePokemon(data.results.slice(startIndex, endIndex))
+    })
+      .catch(error => console.error(error));
+  }, []);
+
+  const nextPage = () => {
+    setCurrentPage(currentPage + 1);
+    setVisiblePokemon(pokemonData.slice(startIndex, endIndex))
+  };
+
+  const prevPage = () => {
+    setCurrentPage(currentPage - 1);
+    setVisiblePokemon(pokemonData.slice(startIndex, endIndex))
+  };
+
+  const startIndex = currentPage * pokemonPerPage;
+  const endIndex = startIndex + pokemonPerPage;
+   
+  console.log(pokemonData)
+
+  return (
+    <div className="frontPage">
+        <div className="pokedexDisplay">
+            {visiblePokemon.map(pokemon => (
+                <PokemonBox key={pokemon.name} id={pokemon} />
+            ))}
+        </div>
+        <div className="displayButtons">
+            {currentPage > 0 && <button onClick={prevPage}>Prev</button>}
+            {endIndex < pokemonData.length && <button onClick={nextPage}>Next</button>}
+        </div>
     </div>
-    );
+  );
 }
--- src/PokemonBox.js
+++ src/PokemonBox.js
@@ -2,14 +2,16 @@ import React, { useState, useEffect } from 'react';
 
 import "./frontpage.css";
 
-
+//inpt name.name is an ID (form 1 to how many pokemins there is)
 
 export default function PokemonBox(name) {
     const [pokemonName, setPokemonName] = useState('');
     const [pokemonType, setPokemonType] = useState('');
     const [pokemonPictureSrc, setPokemonPic] = useState('');
+
+    console.log(name.id.name)
     useEffect(() => {
-        fetch('https://pokeapi.co/api/v2/pokemon/' + name.name)
+        fetch("https://pokeapi.co/api/v2/pokemon/"+ name.id.name)
           .then(response => response.json())
           .then(pokemon => {
             setPokemonType(pokemon.types[0].type.name)
@@ -18,31 +20,15 @@ export default function PokemonBox(name) {
           })
           .catch(error => console.error(error));
       }, []);
-      console.log(pokemonType)
     return(
-            <div className={`pokemonBox ${pokemonType}`}> 
-                <h3>{pokemonName}</h3>
+            <div className={`pokemonBox ${pokemonType}`}>
+                <div>
+                <p>#{name.id.url.substring(34)}</p>
+                    <h3>{pokemonName}</h3>
+                </div>
                 <img src={pokemonPictureSrc}></img>
             </div>
     )
 }
 
-
-
-
-
-
-function getPokemonType(name){
-    fetch("https://pokeapi.co/api/v2/pokemon/" +name.name)//name is an object !!!!
-    .then(response => {
-        if (response.ok) return response.json()
-        throw new Error("Network response was not OK")
-    })
-    .then(pokemon => updateType(pokemon))
-    .catch(err => console.log(err))
-}
-
-function updateType(pokemon){
-    type = pokemon.types[0].type.name;
-}
 
